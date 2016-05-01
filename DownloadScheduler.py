b06d9c0e4ba0a61a8ca19df5323a04fe1f821d53
leonardoalbuquerque <leonardo.lopes.albuquerque@gmail.com>
2016-05-01 15:58:50 -0300

    changed download schedule

--- DownloadScheduler.py
+++ DownloadScheduler.py
@@ -15,10 +15,10 @@ class DownloadScheduler():
 
         cron = Scheduler(daemon=True)
         cron.start()
-        #thread.start_new_thread(main.start, ())
+        thread.start_new_thread(main.start, ())
 
 
-        @cron.interval_schedule(seconds=1)
+        @cron.interval_schedule(hours=1)
         def job_function():
             # Do your work here
             main.start()
